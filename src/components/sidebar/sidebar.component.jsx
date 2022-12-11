3cf5650e9e516241d0f71bb1017511261521774c
didis-tech <paschalanagha@gmail.com>
2022-12-11 10:55:17 +0100

    updated sidebar

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -41,7 +41,7 @@ const Sidebar = () => {
                 <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
               </svg>
             </span>
-            <span className="logo-text">My-Task</span>
+            <span className="logo-text">My-Assistant</span>
           </a>
           {/* Menu: main ul */}
           <ul className="menu-list flex-grow-1 mt-3">
